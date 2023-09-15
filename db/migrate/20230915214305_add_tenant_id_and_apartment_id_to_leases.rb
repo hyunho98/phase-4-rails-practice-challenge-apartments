7f88d021b40403756fb332d2c38cef10af0cede2
Alex Lee <hyunho98@gmail.com>
2023-09-15 18:18:09 -0400

    commit tree 63480009dae06b225c205c9e257ad77f8ef4bc7a

---
+++ db/migrate/20230915214305_add_tenant_id_and_apartment_id_to_leases.rb
@@ -0,0 +1,6 @@
+class AddTenantIdAndApartmentIdToLeases < ActiveRecord::Migration[6.1]
+  def change
+    add_column :leases, :tenant_id, :integer
+    add_column :leases, :apartment_id, :integer
+  end
+end
